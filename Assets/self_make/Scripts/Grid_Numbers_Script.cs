489e35207f57cab38da44b3edf0e7199184de468
PigPig1219 <jason20021219@gmail.com>
2024-10-22 00:43:12 +0800

    0.0.8a
    24.10.22
    在Grid_Numbers_Scrpit上面
    新增了兩個函式
    
    resetNewNumber()
    updateNewNumber()

--- Assets/self_make/Scripts/Grid_Numbers_Script.cs
+++ Assets/self_make/Scripts/Grid_Numbers_Script.cs
@@ -7,13 +7,20 @@ using UnityEngine.UI;
 
 public class Grid_Numbers_Script : MonoBehaviour
 {
-    
+
     /*
     private int[] gridNumber = { 1, 3, 5, 7, 9 };
     private char[] gridSign = { '+', '-', '*', '/' };
     */
+    static private char[] choseSign = { '+', '-', '*', '/' };
     private string[] originalContent = {"4", "/", "5", "-", "3", "*", "1", "+", "2"};
     private string[] gridContent = new string[9];
+
+    private int steps;
+    private int step;
+    private int target;//wait for produce
+    private int[] numbers=new int[5];
+    private char[] signs= new char[4];
 
     //private GameObject gridFather;
     public string getGridContent(int square) {
@@ -34,6 +41,31 @@ public class Grid_Numbers_Script : MonoBehaviour
         }
     }
 
+    //restet signs,step,steps,numbers
+    private void resetNewNumber()
+    {
+        steps = Random.Range(1,2);
+        step = Random.Range(1,3)*3;
+
+        foreach(int i in numbers)
+            numbers[i]=Random.Range(1,9);
+
+        foreach (int i in signs)
+            signs[i] = choseSign[Random.Range(0, 3)];
+    }
+
+    //undateNewNumber to originalContent
+    private void updateNewNumber()
+    {
+        for (int i=0;i<9;i++)
+        {
+            if (i % 2 == 0)
+                originalContent[i] = numbers[i - i / 2].ToString();
+            else
+                originalContent[i] = signs[i - i / 2 - 1].ToString();
+        }
+    }
+
     public void resetGrid() {
         for (int i = 0 ; i < 9 ; ++i) {
             gridContent[i] = originalContent[i];
@@ -45,6 +77,8 @@ public class Grid_Numbers_Script : MonoBehaviour
         gridContent[pos] = value;
         updateGrid();
     }
+
+
     
     private void Awake() {
         
