a9f4e0306099f885a094048fb5cbf18dc0f54d2b
PigPig1219 <jason20021219@gmail.com>
2024-11-27 23:07:19 +0800

    v0.1.3b
    24.11.27
    修改了ui設計
    將文稿劇本分文三個章節作為展示
    
    未完成:
    將遊戲sence與對話場景sence做切換
    將再次修改UI

---
+++ Assets/self_make/Scripts/UI_Script/ControlUI.cs
@@ -0,0 +1,47 @@
+using LLMUnitySamples;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+using Fungus;
+
+public class ControlUI : MonoBehaviour
+{
+
+    private GameObject mainMenu;
+    public void hideMainUI()
+    {
+        mainMenu.SetActive(false);
+    }
+    public void showMainUI()
+    {
+        mainMenu.SetActive(true);
+    }
+
+    public void choseSatge(int value)
+    {
+        switch(value)
+        {
+            case 0:
+                //Fungus.Flowchart.BroadcastFungusMessage("第一幕發生事件");
+                break;
+            case 1:
+                //Fungus.Flowchart.BroadcastFungusMessage("第一幕發生事件(2)");
+                break;
+            case 2:
+               // Fungus.Flowchart.BroadcastFungusMessage("第一幕發生事件(3)");
+                break;
+        }
+    }
+
+    void Start()
+    {
+        mainMenu = GameObject.Find("主畫面選單");
+    }
+
+    void Update()
+    {
+        
+    }
+}
