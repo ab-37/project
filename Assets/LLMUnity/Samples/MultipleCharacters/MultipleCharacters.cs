b2fb3ecaec3dd676c989ceabbe0da95293796e53
coneftinhc <coneftinhc0413@gmail.com>
2024-11-28 04:48:54 +0800

    v0.1.3d
    刪除 Character改用MultipleCharacters
    新增 儲存角色回復的文本

--- Assets/LLMUnity/Samples/MultipleCharacters/MultipleCharacters.cs
+++ Assets/LLMUnity/Samples/MultipleCharacters/MultipleCharacters.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using LLMUnity;
 using UnityEngine.UI;
+using System.IO;
 
 
 namespace LLMUnitySamples
@@ -11,11 +12,14 @@ namespace LLMUnitySamples
         Text AIText;
         LLMCharacter llmCharacter;
 
-        public MultipleCharactersInteraction(InputField playerText, Text AIText, LLMCharacter llmCharacter)
+        string outputFilePath;
+
+        public MultipleCharactersInteraction(InputField playerText, Text AIText, LLMCharacter llmCharacter, string characterName)
         {
             this.playerText = playerText;
             this.AIText = AIText;
             this.llmCharacter = llmCharacter;
+            outputFilePath = characterName + "_Response.txt";
         }
 
         public void Start()
@@ -34,6 +38,8 @@ namespace LLMUnitySamples
         public void SetAIText(string text)
         {
             AIText.text = text;
+            //Debug.Log(text);
+            WriteResponseToFile(text);
         }
 
         public void AIReplyComplete()
@@ -41,6 +47,27 @@ namespace LLMUnitySamples
             playerText.interactable = true;
             playerText.Select();
             playerText.text = "";
+        }
+
+        private void WriteResponseToFile(string response)
+        {
+            try
+            {
+                if (File.Exists(outputFilePath))
+                {
+                    File.WriteAllText(outputFilePath, "");  // 清空文件
+                }
+                using (StreamWriter writer = new StreamWriter(outputFilePath, false))  // 設定為 false，表示不附加內容，直接覆蓋
+                {
+                    // 寫入新的回應，這樣前面的訊息將被覆蓋
+                    writer.WriteLine("AI Response: " + response);
+                    writer.WriteLine("--------");  // 用來區分每次回應
+                }
+            }
+            catch (IOException e)
+            {
+                Debug.LogError("Failed to write to file: " + e.Message);
+            }
         }
     }
 
@@ -56,20 +83,30 @@ namespace LLMUnitySamples
         public Text AIText2;
         MultipleCharactersInteraction interaction2;
 
+        public LLMCharacter llmCharacter3;
+        public InputField playerText3;
+        public Text AIText3;
+        MultipleCharactersInteraction interaction3;
+
         void Start()
         {
-            interaction1 = new MultipleCharactersInteraction(playerText1, AIText1, llmCharacter1);
-            interaction2 = new MultipleCharactersInteraction(playerText2, AIText2, llmCharacter2);
+            interaction1 = new MultipleCharactersInteraction(playerText1, AIText1, llmCharacter1, "Character1");
+            interaction2 = new MultipleCharactersInteraction(playerText2, AIText2, llmCharacter2, "Character2");
+            interaction3 = new MultipleCharactersInteraction(playerText3, AIText3, llmCharacter3, "Character3");
             interaction1.Start();
             interaction2.Start();
+            interaction3.Start();
         }
 
         public void CancelRequests()
         {
             llmCharacter1.CancelRequests();
             llmCharacter2.CancelRequests();
+            llmCharacter3.CancelRequests();
+
             interaction1.AIReplyComplete();
             interaction2.AIReplyComplete();
+            interaction3.AIReplyComplete();
         }
 
         public void ExitGame()
