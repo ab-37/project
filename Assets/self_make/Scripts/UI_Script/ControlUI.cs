c50273a0586542e10aa9097005a4f5ac8bfa0c22
PigPig1219 <jason20021219@gmail.com>
2024-12-27 20:43:25 +0800

    v0.1.5c
    24.12.27
    新增stage json檔
    位置在:
    assert/self_make/stage/
    
    導入 litJson.dll
    
    在controlUI裡面新增搜尋StageID的function

--- Assets/self_make/Scripts/UI_Script/ControlUI.cs
+++ Assets/self_make/Scripts/UI_Script/ControlUI.cs
@@ -6,11 +6,16 @@ using UnityEngine;
 using UnityEngine.UI;
 using Fungus;
 using UnityEngine.SceneManagement;
+using LitJson;
+using System.IO;
 
 public class ControlUI : MonoBehaviour
 {
     public Flowchart flowchart;
     public string blockSelect;
+
+    private string LoadData;
+    private JsonData StageData;
 
     private GameObject mainMenu;
     public void hideMainUI()
@@ -55,13 +60,29 @@ public class ControlUI : MonoBehaviour
         flowchart.ExecuteBlock(blockName);
     }
     
+    private JsonData selcetStage(string chosen_id)
+    {
+        for(int i=0;i< StageData["stage"].Count;i++)
+        {
+            if(chosen_id == StageData["stage"][i]["id"].ToString())
+                return StageData["stage"][i];
+        }
+        return null;
+    }
     
     void Start()
     {
+        LoadData = File.ReadAllText(Application.dataPath + "/self_make/stage/stage.json");
+        StageData = JsonMapper.ToObject(LoadData);
+
         blockSelect = Static_Variables.blockSelect;
         mainMenu = GameObject.Find("MainUI");
 
-        if (blockSelect != null)
+
+        //Debug.Log(StageData["stage"][1]["next_stage"]);
+        Debug.Log(selcetStage("11")["next_stage"]);
+
+        if (blockSelect != "0")
             playBlockStage(blockSelect);
         else
             Debug.Log("no block select");
